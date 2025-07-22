c1f6ac7890ea1022dfe3870ffaa6fc212777d9fc
sashaboriskin <brskn.lx@gmail.com>
2025-07-22 18:05:47 +0300

    Add tools to mcp server

--- mle_kit_mcp/server.py
+++ mle_kit_mcp/server.py
@@ -2,11 +2,22 @@ import fire  # type: ignore
 import uvicorn
 from mcp.server.fastmcp import FastMCP
 
-#from .tools.arxiv_search import arxiv_search
+from .tools.bash import bash
+from .tools.text_editor import text_editor
+from .tools.remote_gpu import (
+    remote_bash,
+    create_remote_text_editor,
+    remote_download,
+)
+
 
 server = FastMCP("MLE kit MCP", stateless_http=True)
 
-#server.add_tool(arxiv_search)
+server.add_tool(bash)
+server.add_tool(text_editor)
+server.add_tool(remote_bash)
+server.add_tool(create_remote_text_editor)
+server.add_tool(remote_download)
 
 http_app = server.streamable_http_app()
 
