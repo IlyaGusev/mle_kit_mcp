a472cc11280e1c5c39c762cf42bb7592c22eee4d
Ilya Gusev <phoenixilya@gmail.com>
2025-10-04 20:18:56 +0000

    Fix

--- mle_kit_mcp/files.py
+++ mle_kit_mcp/files.py
@@ -4,6 +4,7 @@ from mle_kit_mcp.settings import settings
 
 
 def get_workspace_dir() -> Path:
+    assert settings.WORKSPACE_DIR is not None, "Please set the WORKSPACE_DIR environment variable"
     directory = Path(settings.WORKSPACE_DIR)
     if not directory.exists():
         directory.mkdir(parents=True, exist_ok=True)
